10c168166afa2711f74dfb04eb2b99a98d47afeb
Sungmo-Ku <sungmo5845_@naver.com>
2021-08-12 00:59:32 +0900

    final

--- app/src/main/java/com/example/contact_esc_2021/Adapter.java
+++ app/src/main/java/com/example/contact_esc_2021/Adapter.java
@@ -1,10 +1,17 @@
 package com.example.contact_esc_2021;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.content.ContentProviderOperation;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.OperationApplicationException;
+import android.database.Cursor;
 import android.net.Uri;
+import android.os.RemoteException;
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,7 +38,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.Holder>{
     @NonNull
     @org.jetbrains.annotations.NotNull
     @Override
-    public Holder onCreateViewHolder(@NonNull @org.jetbrains.annotations.NotNull ViewGroup parent, int viewType) {
+    public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.recy_item, parent, false);
         Holder holder = new Holder(view);
         return holder;
@@ -39,7 +46,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.Holder>{
     }
 
     @Override
-    public void onBindViewHolder(@NonNull @org.jetbrains.annotations.NotNull Adapter.Holder holder, int position) {
+    public void onBindViewHolder(@NonNull Holder holder, int position) {
         Contact contact = datalist.get(position);
         holder.name.setText(contact.getName());
         holder.phonenum.setText(contact.getPhoneNumber());
@@ -69,7 +76,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.Holder>{
                         intent.putExtra("address", contact.getPhoneNumber());
                         intent.putExtra("sms_body", "");
                         intent.setType("vnd.android-dir/mms-sms");
-                        context.startActivity(call.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                        context.startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
 
 
                     }
@@ -88,7 +95,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.Holder>{
                 builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        //핸드폰에서 연락처 지우기 해야 함.
+                        deleteContactFromNumber(context.getContentResolver(), datalist.get(position).getPhoneNumber());
                         datalist.remove(position);
                         notifyItemRemoved(position);
                         notifyItemRangeChanged(position, datalist.size());
@@ -110,7 +117,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.Holder>{
             protected TextView name;
         protected TextView phonenum;
         protected Button call;
-        public Holder(@NonNull @org.jetbrains.annotations.NotNull View itemView) {
+        public Holder(@NonNull View itemView) {
             super(itemView);
 
             item_container = itemView.findViewById(R.id.item_container);
@@ -119,4 +126,40 @@ public class Adapter extends RecyclerView.Adapter<Adapter.Holder>{
             call = itemView.findViewById(R.id.call);
         }
     }
+
+    public void filterList(ArrayList<Contact> filteredList) {
+            datalist = filteredList;
+            notifyDataSetChanged();
+    }
+
+    @SuppressLint("Range")
+    private static long getContactIdFromNumber(ContentResolver contactHelper, String number) {
+            Uri contactUri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(number));
+
+            String[] projection = {ContactsContract.PhoneLookup._ID};
+
+            Cursor cursor = contactHelper.query(contactUri, projection, null, null, null);
+
+            if(cursor.moveToFirst()){
+               return cursor.getLong(cursor.getColumnIndex(ContactsContract.PhoneLookup._ID));
+        }else if (cursor != null) {
+                cursor.close();
+            }
+            return -1;
+    }
+         public static void deleteContactFromNumber(ContentResolver contactHelper, String number) {
+            ArrayList<ContentProviderOperation> ops = new ArrayList<>();
+            String[] WhereArgs = new String[] {String.valueOf(getContactIdFromNumber(contactHelper, number))};
+
+            ops.add(ContentProviderOperation.newDelete(ContactsContract.RawContacts.CONTENT_URI).
+                    withSelection(ContactsContract.RawContacts.CONTACT_ID + "=?", WhereArgs).build());
+
+            try {
+                contactHelper.applyBatch(ContactsContract.AUTHORITY, ops);
+            }catch (OperationApplicationException e) {
+                e.printStackTrace();
+            }catch (RemoteException e) {
+                e.printStackTrace();
+            }
+         }
 }
